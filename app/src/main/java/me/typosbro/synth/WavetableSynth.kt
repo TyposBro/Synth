93760254000253df18d2ca1bb841d23b449fb21f
typosbro <typosbro@proton.me>
2024-03-18 16:42:27 +0900

    Create WavetableSynth.kt

---
+++ app/src/main/java/me/typosbro/synth/WavetableSynth.kt
@@ -0,0 +1,39 @@
+package me.typosbro.synth
+
+import androidx.annotation.StringRes
+
+enum class Wavetable {
+    SINE {
+        override fun toResourceString(): Int {
+            return R.string.sine
+        }
+    },
+    TRIANGLE {
+        override fun toResourceString(): Int {
+            return R.string.triangle
+        }
+    },
+    SQUARE {
+        override fun toResourceString(): Int {
+            return R.string.square
+        }
+    },
+    SAWTOOTH {
+        override fun toResourceString(): Int {
+            return R.string.sawtooth
+        }
+    };
+
+    @StringRes
+    abstract fun toResourceString(): Int
+}
+
+interface WavetableSynth {
+    suspend fun play(wavetable: Wavetable, frequency: Float, duration: Long)
+    suspend fun stop()
+    suspend fun isPlaying(): Boolean
+    suspend fun setFrequency(frequency: Float)
+    suspend fun setVolume(volume: Float)
+    suspend fun setWavetable(wavetable: Wavetable)
+
+}
