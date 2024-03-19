5dee189e46ebe8ccbe334755f2f0b26685d3cd05
typosbro <typosbro@proton.me>
2024-03-19 15:06:35 +0900

    Create Logger

---
+++ app/src/main/cpp/WavetableSynth.cpp
@@ -0,0 +1,37 @@
+//
+// Created by ched54 on 3/19/2024.
+//
+#include "include/Log.h"
+#include "include/WavetableSynth.h"
+
+
+namespace wavetablesynth {
+
+    void WavetableSynth::play() {
+        LOGD("WavetableSynth::play()");
+        _isPlaying = true;
+    }
+
+    void WavetableSynth::stop() {
+        LOGD("WavetableSynth::stop()");
+        _isPlaying = false;
+    }
+
+    bool WavetableSynth::isPlaying() {
+        LOGD("WavetableSynth::isPlaying()");
+        return _isPlaying;
+    }
+
+    void WavetableSynth::setFrequency(float frequency) {
+        LOGD("WavetableSynth::setFrequency() called with %.2f", frequency);
+    }
+
+    void WavetableSynth::setVolume(float volume) {
+        LOGD("WavetableSynth::setVolume() called with %.2f", volume);
+    }
+
+    void WavetableSynth::setWaveform(Waveform waveform) {
+        LOGD("WavetableSynth::setWaveform() called with %d", static_cast<int>(waveform));
+    }
+
+}
