4c5cb09b1cdfa7f46e42142883ccaf8fa47fdb64
typosbro <typosbro@proton.me>
2024-03-19 15:06:10 +0900

    implement methods

--- app/src/main/cpp/synth.cpp
+++ app/src/main/cpp/synth.cpp
@@ -1,4 +1,8 @@
 #include <jni.h>
+#include <memory>
+#include "include/Log.h"
+#include "include/WavetableSynth.h"
+
 
 // Write C++ code here.
 //
@@ -17,46 +21,99 @@
 //         System.loadLibrary("synth")
 //      }
 //    }
-extern "C"
+extern "C" {
 JNIEXPORT jlong JNICALL
 Java_me_typosbro_synth_NativeWavetableSynth_create(JNIEnv *env, jobject thiz) {
-    // TODO: implement create()
+    auto synth = std::make_unique<::WavetableSynth>();
+    if (not synth) {
+        //LOGD("Failed to create WavetableSynth");
+        synth.reset(nullptr);
+    }
+    return reinterpret_cast<jlong>(synth.release());
 }
 extern "C"
 JNIEXPORT void JNICALL
 Java_me_typosbro_synth_NativeWavetableSynth_destroy(JNIEnv *env, jobject thiz, jlong handle) {
-    // TODO: implement destroy()
+    auto *synth = reinterpret_cast<wavetablesynth::WavetableSynth>(handle);
+
+    if (not synth) {
+        // LOGD("WavetableSynth is null");
+        return;
+    }
+    delete synth;
 }
 extern "C"
 JNIEXPORT void JNICALL
 Java_me_typosbro_synth_NativeWavetableSynth_play(JNIEnv *env, jobject thiz, jlong handle) {
-    // TODO: implement play()
+    auto *synth = reinterpret_cast<wavetablesynth::WavetableSynth>(handle);
+
+    if (not synth) {
+        // LOGD("Synth not created");
+        return;
+    }
+    synth->play();
+
 }
 extern "C"
 JNIEXPORT void JNICALL
 Java_me_typosbro_synth_NativeWavetableSynth_stop(JNIEnv *env, jobject thiz, jlong handle) {
-    // TODO: implement stop()
+    auto *synth = reinterpret_cast<wavetablesynth::WavetableSynth>(handle);
+
+    if (not synth) {
+        // LOGD("Synth not created");
+        return;
+    }
+    synth->stop();
 }
 extern "C"
 JNIEXPORT jboolean JNICALL
 Java_me_typosbro_synth_NativeWavetableSynth_isPlaying(JNIEnv *env, jobject thiz, jlong handle) {
-    // TODO: implement isPlaying()
+    auto *synth = reinterpret_cast<wavetablesynth::WavetableSynth>(handle);
+
+    if (not synth) {
+        // LOGD("Synth not created");
+        return false;
+    }
+    return synth->isPlaying();
 }
 extern "C"
 JNIEXPORT void JNICALL
 Java_me_typosbro_synth_NativeWavetableSynth_setFrequency(JNIEnv *env, jobject thiz, jlong handle,
                                                          jfloat frequency) {
-    // TODO: implement setFrequency()
+    auto *synth = reinterpret_cast<wavetablesynth::WavetableSynth>(handle);
+
+    if (not synth) {
+        // LOGD("Synth not created");
+        return;
+    }
+    synth->setFrequency(static_cast<float>(frequency));
 }
 extern "C"
 JNIEXPORT void JNICALL
 Java_me_typosbro_synth_NativeWavetableSynth_setVolume(JNIEnv *env, jobject thiz, jlong handle,
                                                       jfloat volume) {
-    // TODO: implement setVolume()
+    auto *synth = reinterpret_cast<wavetablesynth::WavetableSynth>(handle);
+
+
+    if (not synth) {
+        // LOGD("Synth not created");
+        return;
+    }
+    synth->setVolume(static_cast<float>(volume));
 }
 extern "C"
 JNIEXPORT void JNICALL
 Java_me_typosbro_synth_NativeWavetableSynth_setWavetable(JNIEnv *env, jobject thiz, jlong handle,
                                                          jint wavetable) {
-    // TODO: implement setWavetable()
+
+    auto *synth = reinterpret_cast<wavetablesynth::WavetableSynth>(handle);
+    const auto nativeWaveTable = static_cast<wavetablesynth::Wavetable>(wavetable);
+
+
+    if (not synth) {
+        // LOGD("Synth not created");
+        return;
+    }
+    synth->setWavetable(static_cast<wavetablesynth::Wavetable>(wavetable));
+}
 }
