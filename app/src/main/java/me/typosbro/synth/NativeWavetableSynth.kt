156b78f6abe1fe2fe42dcffc2cdb92cadf3d67ef
typosbro <typosbro@proton.me>
2024-03-19 12:38:11 +0900

    handle library lifecycle

--- app/src/main/java/me/typosbro/synth/NativeWavetableSynth.kt
+++ app/src/main/java/me/typosbro/synth/NativeWavetableSynth.kt
@@ -1,10 +1,11 @@
 package me.typosbro.synth
 
 import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 
 class NativeWavetableSynth : WavetableSynth, DefaultLifecycleObserver {
     // Handle to native wavetable synth class that holds memory address of native object
-    private val nativeWavetableSynthHandle: Long = 0
+    private var nativeWavetableSynthHandle: Long = 0
 
     // Make access to native wavetable synth class thread-safe, so we don't use our synth when it's being destroyed
     private val synthMutex = Object()
@@ -23,6 +24,24 @@ class NativeWavetableSynth : WavetableSynth, DefaultLifecycleObserver {
     companion object {
         init {
             System.loadLibrary("wavetablesynthesizer")
+        }
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+        super.onResume(owner)
+        synchronized(synthMutex) {
+            createNativeHandle()
+        }
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+        super.onPause(owner)
+        synchronized(synthMutex) {
+            if (nativeWavetableSynthHandle == 0L){
+                return
+            }
+            destroy(nativeWavetableSynthHandle)
+            nativeWavetableSynthHandle = 0L
         }
     }
 
@@ -49,5 +68,13 @@ class NativeWavetableSynth : WavetableSynth, DefaultLifecycleObserver {
 
     override suspend fun setWavetable(wavetable: Wavetable) {
         TODO("Not yet implemented")
+    }
+
+    private fun createNativeHandle() {
+        if (nativeWavetableSynthHandle != 0L) {
+            return
+        }
+        // Create native wavetable synth object and store its memory address in nativeWavetableSynthHandle
+        nativeWavetableSynthHandle = create()
     }
 }
